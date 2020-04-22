b4f82db5ca5bd10aba595321569ca0a12ef17f1b
s.stetsenko <s.stetsenko@ceteralabs.com>
2020-04-22 12:13:42 +0300

    Добавил новые кнопки в спиниги настроил редиректы, внес в гит инклюды

--- www/crazy-fish.biz/include/catalog_footer_btn.php
+++ www/crazy-fish.biz/include/catalog_footer_btn.php
@@ -1,6 +1,6 @@
+<a class="catalog_footer_button" href="/catalog/tvich/spinning_tvich/">Для твичинга</a>
 <a class="catalog_footer_button" href="/catalog/jig/spinning_jig/">Для джига</a>
 <a class="catalog_footer_button" href="/catalog/mikrojig/spinning_mikrojig/">Для микроджига</a>
 <a class="catalog_footer_button" href="/catalog/forel/spinning_forel/">Для форели</a>
 <a class="catalog_footer_button" href="/catalog/nanojig/spinning_nanojig/">Для наноджига</a>
 <a class="catalog_footer_button" href="/catalog/bigfish/spinning_bigfish/">Для бигфиша</a>
-<a class="catalog_footer_button" href="/catalog/tvich/spinning_tvich/">Для твичинга</a>
